2e1ed4542b8f1b777b3c80a357502b19f6d53d80
Shinnosuke Irie <kiyoshidainagon@gmail.com>
2020-06-21 00:02:56 +0900

    Add style

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template lang="pug">
-  p {{ message }}
+  div
+    p.text-red-700 {{ message }}
+    button(class='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded') button
 </template>
 
 <script lang="ts">
